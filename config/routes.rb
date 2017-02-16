4fe8139dac6bfffc3b5d2ce4ce7005ff05241383
Catherine Oxley <catherine.oxley@live.co.uk>
2017-02-16 12:50:32 +0000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/edit'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/_form'
+
+  resources :cocktails do
+    resources :doses
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+uri = URI.parse("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+
+
+uri.open {|f|
+  # ...
+}

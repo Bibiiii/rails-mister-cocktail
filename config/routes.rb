01bc557d48cc6644f61fd6f02f36d1a55a1e6ff1
Catherine Oxley <catherine.oxley@live.co.uk>
2017-02-16 15:16:45 +0000

    app works with ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
     resources :doses
   end
 
+  root to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

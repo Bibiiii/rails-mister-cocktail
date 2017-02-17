5fe58d0240217f91d6480c76075dd101008ad400
Catherine Oxley <catherine.oxley@live.co.uk>
2017-02-17 17:07:10 +0000

    photo validation added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses, dependent: :restrict_with_exception
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 
   has_attachment :photo
 end
